a80bd2f9de496c7941cd819703ea89c6da4fdea9
Mohamed Adem <mamadem@mun.ca>
2022-07-24 22:48:42 -0230

    comments

--- mhm.py
+++ mhm.py
@@ -3,8 +3,8 @@ import cv2
 from mpi4py import MPI
 
 comm = MPI.COMM_WORLD
-size = comm.Get_size()
-rank = comm.Get_rank()
+size = comm.Get_size()  # number of nodes, in this case 16
+rank = comm.Get_rank()  # gets the current rank
 
 
 def convolve(img, kernel):
@@ -26,46 +26,7 @@ def convolve(img, kernel):
     return conv
 
 
-kernel = np.array(
-    [
-        [0, 0, 3, 2, 2, 2, 3, 0, 0],
-        [0, 2, 3, 5, 5, 5, 3, 2, 0],
-        [3, 3, 5, 3, 0, 3, 5, 3, 3],
-        [2, 5, 3, -12, -23, -12, 3, 5, 2],
-        [2, 5, 0, -23, -40, -23, 0, 5, 2],
-        [2, 5, 3, -12, -23, -12, 3, 5, 2],
-        [3, 3, 5, 3, 0, 3, 5, 3, 3],
-        [0, 2, 3, 5, 5, 5, 3, 2, 0],
-        [0, 0, 3, 2, 2, 2, 3, 0, 0],
-    ]
-)
-
-filepath = "pepper.ascii.pgm"
-image = cv2.imread(filepath, 0)
-
-M = 64
-N = 64
-
-if rank == 0:
-    data = [
-        image[x : x + M, y : y + N]
-        for x in range(0, image.shape[0], M)
-        for y in range(0, image.shape[1], N)
-    ]
-else:
-    data = None
-
-data = comm.scatter(data, root=0)
-data = convolve(data, kernel)
-print("rank", rank, "has data:", data)
-
-convolvedImage = comm.gather(data, root=0)
-
-if rank == 0:
-    cv2.imshow("image", combineTilesIntoOneImage(convolvedImage))
-    cv2.waitKey(5000)
-
-
+# This method takes an array of the 16 convoluted 64x64 tiles, and combines them to the final image
 def combineTilesIntoOneImage(convolvedImage):
     im_v = cv2.hconcat(
         [convolvedImage[0], convolvedImage[1], convolvedImage[2], convolvedImage[3]]
@@ -81,3 +42,55 @@ def combineTilesIntoOneImage(convolvedImage):
     )
 
     return cv2.vconcat([im_v, im_v2, im_v3, im_v4])
+
+
+# given kernel matrix
+kernel = np.array(
+    [
+        [0, 0, 3, 2, 2, 2, 3, 0, 0],
+        [0, 2, 3, 5, 5, 5, 3, 2, 0],
+        [3, 3, 5, 3, 0, 3, 5, 3, 3],
+        [2, 5, 3, -12, -23, -12, 3, 5, 2],
+        [2, 5, 0, -23, -40, -23, 0, 5, 2],
+        [2, 5, 3, -12, -23, -12, 3, 5, 2],
+        [3, 3, 5, 3, 0, 3, 5, 3, 3],
+        [0, 2, 3, 5, 5, 5, 3, 2, 0],
+        [0, 0, 3, 2, 2, 2, 3, 0, 0],
+    ]
+)
+
+# read the input file
+filepath = "pepper.ascii.pgm"
+image = cv2.imread(filepath, 0)
+
+M = 64
+N = 64
+
+if rank == 0:
+    # divide the input image into 16 tiles of 64x64
+    data = [
+        image[x : x + M, y : y + N]
+        for x in range(0, image.shape[0], M)
+        for y in range(0, image.shape[1], N)
+    ]
+else:
+    data = None
+
+# MPI_SCATTER to scatter the 16 tiles among the 16 nodes
+data = comm.scatter(data, root=0)
+
+# Convolve the tile using the given kernel
+data = convolve(data, kernel)
+print("rank", rank, "has data:", data)
+
+# MPI_GATHER to gather all 16 individual convolved tiles into one array
+convolvedImage = comm.gather(data, root=0)
+
+if rank == 0:
+    # combine the tiles and show the image for 5 seconds
+    final_output = combineTilesIntoOneImage(convolvedImage)
+    cv2.imshow("image", final_output)
+    cv2.waitKey(5000)
+
+    # write the final output into a png file
+    cv2.imwrite("final_output.png", final_output)
